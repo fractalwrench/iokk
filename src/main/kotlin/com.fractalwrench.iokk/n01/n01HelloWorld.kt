162601972e4260b4c6b819b427ef3e34a9e08d90
fractalwrench <fractalwrench@gmail.com>
2018-07-23 21:38:44 +0100

    add challenge docs

--- src/main/kotlin/com.fractalwrench.iokk/n01/n01HelloWorld.kt
+++ src/main/kotlin/com.fractalwrench.iokk/n01/n01HelloWorld.kt
@@ -1,5 +1,50 @@
 package com.fractalwrench.iokk.n01
 
+/**
+
+###
+#  #    # ##### ###### #####  #    #   ##   ##### #  ####  #    #   ##   #
+#  ##   #   #   #      #    # ##   #  #  #    #   # #    # ##   #  #  #  #
+#  # #  #   #   #####  #    # # #  # #    #   #   # #    # # #  # #    # #
+#  #  # #   #   #      #####  #  # # ######   #   # #    # #  # # ###### #
+#  #   ##   #   #      #   #  #   ## #    #   #   # #    # #   ## #    # #
+### #    #   #   ###### #    # #    # #    #   #   #  ####  #    # #    # ######
+
+#######
+#     # #####  ###### #    #  ####   ####    ##   ##### ###### #####
+#     # #    # #      #    # #      #    #  #  #    #   #      #    #
+#     # #####  #####  #    #  ####  #      #    #   #   #####  #    #
+#     # #    # #      #    #      # #      ######   #   #      #    #
+#     # #    # #      #    # #    # #    # #    #   #   #      #    #
+####### #####  #       ####   ####   ####  #    #   #   ###### #####
+
+#    #
+#   #   ####  ##### #      # #    #
+#  #   #    #   #   #      # ##   #
+###    #    #   #   #      # # #  #
+#  #   #    #   #   #      # #  # #
+#   #  #    #   #   #      # #   ##
+#    #  ####    #   ###### # #    #
+
+#####
+#     #  ####  #    # ##### ######  ####  #####
+#       #    # ##   #   #   #      #        #
+#       #    # # #  #   #   #####   ####    #
+#       #    # #  # #   #   #           #   #
+#     # #    # #   ##   #   #      #    #   #
+#####   ####  #    #   #   ######  ####    #
+
+
+Challenge 1: Print Hello World
+
+Your program should write the string 'Hello, World!\n' to System.out, in the most convoluted manner
+possible. A passing test is provided.
+
+When you're happy with your solution, open a PR with a copy of the file, with your GitHub username as a suffix for
+the file, class, and method names. For example, n01HelloWorld would become n01HelloWorld_<username>.
+
+Please also document how your monstrosity works using KDoc.
+ */
 fun ch1Solution() {
     println("Hello, World!")
 }
